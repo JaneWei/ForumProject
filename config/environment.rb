414d8b435c43e945057b832cd64cd360259ff94f
System Administrator <root@ywang-osx.silverspringnet.com>
2012-08-03 15:25:05 -0700

    2 test

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ForumWarrior::Application.initialize!
+ 
+ActionMailer::Base.delivery_method = :smtp;
+
+ActionMailer::Base.smtp_settings = {
+:address => "smtp.gmail.com",
+:port => 587,
+:domain => "glacial-dusk-5436.herokuapp.com",
+:authentication => :plain,
+:user_name => "tomatoisfurious",
+:password => "furiousfoobar",
+}
