f5eb3a2d062bf0d90486592f88bd7099a5042604
System Administrator <root@ywang-osx.silverspringnet.com>
2012-08-03 14:11:25 -0700

    try to make it normal

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ ForumWarrior::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
- # resources :password_resets
+  resources :password_resets
 
   root to: 'static_pages#home'
-
 	match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
 	match '/contact', to: 'static_pages#contact'
